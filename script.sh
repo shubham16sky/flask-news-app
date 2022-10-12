8e4a4a909505346d324c09c394fa059145b2b601
shubham16sky <shubhampc16@gmail.com>
2022-10-13 02:19:20 +0530

    changed script file

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ container=`sudo docker ps | awk '{print $NF}' | awk '(NR>1)' | grep -E "testing"
 
 if [ ! -z "$container" ]
 then
-        sudo docker stop testing && sudo docker rm testing
+        sudo docker stop testing && sudo docker rm testing  && sudo docker run --name "testing" --detach  flask-ci-cd
+
 else
         sudo docker run --name "testing" --detach  flask-ci-cd
